e5477d773f6998b4f7b667038021783c0561f4dd
Marta <martadipuccio@me.com>
2018-05-24 13:14:11 +0200

    json files saved in a folder

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-/* {
-  "uuid": "1b671a64-40d5-491e-99b0-da01ff1f3341", //every user should become a uuid. https://www.npmjs.com/package/uuid
-  "email":"tommy@tmy.io", //from form
-  "pw":"123", //from form
-  "status":"unconfirmed", //default value unconfirmed, every register is first unconfirmed
-  "session": true //depending on the RememberMe checkbox
-} */
+/* var fs = require('fs');
+fs.writeFile("/tmp/test", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); */
 
 
 require('dotenv').config()
 const uuidv4 = require('uuid/v4');
 const express = require('express');
-const app = express()
-
-
-
-
-
 var fs = require('fs');
+const app = express()
 
 
 
@@ -35,10 +31,10 @@ app.get('/user/create', (req, res) => {
         "email": req.query.email,
         "pw": req.query.pw, //from form
         "status":"unconfirmed", //default value unconfirmed
-        "session": true //depending on the RememberMe checkbox
+        "session": true  //depending on the RememberMe checkbox
 } 
     console.log(obj)
-    fs.writeFile(`${id}.json`, JSON.stringify(obj), function(err) {
+    fs.writeFile(`./usersFile/${id}.json`, JSON.stringify(obj), function(err) {
         if(err) {
             return console.log(err);
         }
