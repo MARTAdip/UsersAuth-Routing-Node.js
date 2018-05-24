094e7cfb12285b8555b10639a17a3b436995e2f3
Marta <martadipuccio@me.com>
2018-05-24 11:43:19 +0200

    will create a json file for each user created, based on the object i have, these users will have a unique uuid.

--- index.js
+++ index.js
@@ -11,17 +11,9 @@ require('dotenv').config()
 const uuidv4 = require('uuid/v4');
 const express = require('express');
 const app = express()
-uuidv4();
-// uuidv4(options, buffer, offset);
-/* const v4options = {
-  random: [
-    0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea,
-    0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36
-  ]
-};
-uuidv4(v4options) */
 
-//var sample = fs.readFileSync('sample.txt', 'utf8');
+
+
 
 
 var fs = require('fs');
@@ -36,33 +28,23 @@ app.get('/user/create', (req, res) => {
     console.log("Route /user")
     console.log(req.query)
 
-    function generateUUID() {
-    var d = new Date().getTime();
-    if(Date.now){
-        d = Date.now(); //high-precision timer
-    }
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-    });
-    return uuid;
-};
+  
+    var id = uuidv4() // will generate random and all diverse unique ids 
     var obj =  {
-        "uuid": generateUUID(uuid), 
+        "uuid": id,
         "email": req.query.email,
-        "pw":"123", //from form
+        "pw": req.query.pw, //from form
         "status":"unconfirmed", //default value unconfirmed
         "session": true //depending on the RememberMe checkbox
-    } 
-    var file = fs.writeFile("./usersFile/file.json", obj, function(err) {
+} 
+    console.log(obj)
+    fs.writeFile(`${id}.json`, JSON.stringify(obj), function(err) {
         if(err) {
             return console.log(err);
         }
         console.log("The file was saved!");
 }); 
 
-    obj = JSON.stringify(file);
     res.send('user created')
 
 })
